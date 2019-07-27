c2e77f6c51ec7c2eb8fbb1a149af731484934dc2
José Manuel Agúndez García <jmagundezg@gmail.com>
2019-07-27 17:38:59 +0200

    implementing EV3_VREP

---
+++ components/EV3_VREP/src/EV3_LEGO_controller.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import vrep
+from toolkit import *
+from vrep_client_controller import VRepClientController
+import numpy as np
+import math
+__package__ = "EV3_ROBOCOMP_LEGO"
+
+__version__ = "1.0.1"
+__author__ = "José Manuel Agúndez García"
+__license__ = "GPL"
+
+
+class EV3Controller(VRepClientController):
+    
+
+    __COMPONENTS = {
+        'robot': 'LEGO_EV3',
+        'camera': 'Camara',
+        'camera_bumper': 'Camara_bumper',
+        'camera_sonar': 'Camara_sonar',
+        'sensor_color_LR': 'Sensor_Color_LR',
+        'sensor_color_RC': 'Sensor_Color_RC',
+        'sonar': 'Sonar',
+        'bumper': 'Bumper',
+        'motor_b': 'Motor_B',
+        'motor_c': 'Motor_C',
+        'slider': 'Slider_SF',
+        'giroscope': 'Giroscopio'
+    }
+
+    def __init__(self, host, port, suffix="", debug=False):
+        """
+        EV3Controller constructor
+
+        Arguments:
+            host {[string]} -- ip host
+            port {[int]} -- VREP port
+        
+        Keyword Arguments:
+            suffix {str} -- [Suffix for multiple robots] (default: {""})
+            debug {bool} -- [Debug messages activation] (default: {False})
+        """
+        VRepClientController.__init__(self, host, port)
+        self.suffix = suffix
+        self.debug = debug
+        self.components = {}
+        self.handle_objects()
+        self.left_vel = None
+        self.right_vel = None
+        self.position = [None, None, None]
+
+
+    def handle_objects(self):
+        """
+        Connects each component to the simulator
+        """
+        for i, j in EV3Controller.__COMPONENTS.iteritems():
+            self.components[i] = {'name': j + self.suffix, 'id': None}
+
+        for i in self.components.keys():
+            res, comp_id = vrep.simxGetObjectHandle(
+                self.client_id, self.components[i]['name'],
+                vrep.simx_opmode_oneshot_wait)
+            if res == 0:
+                self.components[i]['id'] = comp_id
+            elif res != 0 and self.debug:
+                err_print(prefix="HANDLE OBJECTS:" +
+                          self.components[i]['name'] + " ",
+                          message=parse_error(res))
+
+
+    def set_speed_left(self, speed):
+        """Sets the speed of the left motor
+        
+        Arguments:
+            speed {[int]} -- [Linear speed of the left motor, it should in [-1000, 1000]]
+        
+        Returns:
+            res[int] -- [result of the vrep library method]
+            err_list[list[string]] -- list of errors
+        """
+        if speed > 1000:
+            speed = 1000
+        elif speed < -1000:
+            speed = -1000
+        res = 0
+        err_list = []
+        if self.left_vel != speed:
+            res = -1
+            if self.components['motor_c']['id'] != None:
+                res = vrep.simxSetJointTargetVelocity(
+                    self.client_id, self.components['motor_c']['id'], speed,
+                    vrep.simx_opmode_streaming)
+            err_list = parse_error(res)
+            if res != 0 and self.debug:
+                err_print(prefix="SET SPEED LEFT: ", message=err_list)
+        return res, err_list
+
+    def set_speed_right(self, speed):
+        """Sets the speed of the right motor
+        
+        Arguments:
+            speed {[int]} -- [Linear speed of the right motor, it should in [-1000, 1000]]
+        
+        Returns:
+            res[int] -- [result of the vrep library method]
+            err_list[list[string]] -- list of errors
+        """
+        if speed > 1000:
+            speed = 1000
+        elif speed < -1000:
+            speed = -1000
+        err_list = []
+        res = 0
+        if self.right_vel != speed:
+            res = -1
+            if self.components['motor_b']['id'] != None:
+                res = vrep.simxSetJointTargetVelocity(
+                    self.client_id, self.components['motor_b']['id'], speed,
+                    vrep.simx_opmode_streaming)
+                err_list = parse_error(res)
+                if res != 0 and self.debug:
+                    err_print(prefix="SET SPEED RIGHT: ", message=err_list)
+        return res, err_list
+
+
+    def get_light_sensors(self):
+        """ Light sensors data getter
+        
+        Returns:
+            [list] -- [list of light sensors data]
+        """
+        res_list = []
+        for i in list(filter(lambda x: 'sensor_color' in x[0] ,self.components.items())):
+            res, _, prox = vrep.simxReadVisionSensor(self.client_id, i[1]['id'], vrep.simx_opmode_blocking)
+            if res != vrep.simx_return_ok:
+                err_print(prefix="GET LIGHT SENSORS: ", message=err_list)
+            res_list.append(prox[0][11:14])
+        return res_list
+
+    def get_camera_image(self):
+        data = {'res' : -1,
+                'err_list': ["camera isn't connected to client"],
+                'data': {'image': np.zeros((128, 128, 3)), 'resolution': (128, 128, 3)}}
+        if self.components['camera']['id'] != None:
+            res, resolution, image = vrep.simxGetVisionSensorImage(self.client_id, self.components['camera']['id'], 0, vrep.simx_opmode_streaming)
+            data['res'] = res
+            data['err_list'] = err_list = parse_error(res)
+            print resolution
+            if res not in (0, 1) and self.debug:
+                err_print(prefix='CAMERA', message=err_list)
+            elif len(resolution) > 1:
+                resolution = (resolution[0], resolution[1], 3)
+                data['data']['resolution'] = resolution 
+                mat_image = np.array(image, dtype=np.uint8)
+                mat_image.resize([resolution[0], resolution[1], 3])
+
+                data['data']['image'] = mat_image[::-1]
+        return data 
+
+                
+    def get_base_pose(self):
+        res, pos = vrep.simxGetObjectPosition(self.client_id, self.components['robot']['id'], -1, vrep.simx_opmode_blocking)
+        if res != 0:
+            err_print("GET BASE POSE", parse_error(res))
+            raise Exception("ERROR IN GET BASE POSE")
+        else:
+            res, ang = vrep.simxGetObjectOrientation(self.client_id, self.components['robot']['id'], -1, vrep.simx_opmode_blocking)
+            if res != 0:
+                err_print("GET BASE POSE", parse_error(res))
+                raise Exception("ERROR IN GET BASE POSE")
+            else: 
+                ang = ang[-1]
+                return pos[0], pos[1], ang
+
+    def get_base_pose_full(self):
+        res, pos = vrep.simxGetObjectPosition(self.client_id, self.components['robot']['id'], -1, vrep.simx_opmode_blocking)
+        if res != 0:
+            err_print("GET BASE POSE", parse_error(res))
+            raise Exception("ERROR IN GET BASE POSE")
+        else:
+            res, ang = vrep.simxGetObjectOrientation(self.client_id, self.components['robot']['id'], -1, vrep.simx_opmode_blocking)
+            if res != 0:
+                err_print("GET BASE POSE", parse_error(res))
+                raise Exception("ERROR IN GET BASE POSE")
+            else: 
+                ang = ang[-1]
+                return pos, ang
+    
+
+    def move_robot(self, x, z, alpha):
+        y = self.get_base_pose_full()[0][2]
+        res = vrep.simxSetObjectPosition(self.client_id, self.components['robot']['id'], -1, (x, y, z), vrep.simx_opmode_blocking)
+        if res != 1:
+            err_print("MOVE ROBOT SET POSITION", parse_error(res))
+        
+        # res = vrep.simxSetObjectOrientation(self.client_id, self.components['robot']['id'], -1, (alpha, 0, 0), vrep.simx_opmode_oneshot)
+        # if res != 1:
+        #     err_print("MOVE ROBOT SET ORIENTATION", parse_error(res))
+
+    def set_global_speed(self, adv, rot):
+        """set the robot speed using an advance and rotation speed
+        
+        Arguments:
+            adv {[type]} -- [description]
+            rot {[type]} -- [description]
+        """
+
+        if adv > 100:
+            adv = 100
+        elif adv < -100:
+            adv = -100
+        if rot > math.pi:
+            rot = math.pi
+        elif rot < -math.pi:
+            rot = -math.pi
+
+        
+
---
+++ components/EV3_VREP/src/specificworker.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 by JOSÉ MANUEL AGÚNDEZ GARCÍA
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys, os, traceback, time
+
+from PySide import QtGui, QtCore
+from genericworker import *
+from EV3_LEGO_controller import EV3Controller
+# If RoboComp was compiled with Python bindings you can use InnerModel in Python
+# sys.path.append('/opt/robocomp/lib')
+# import librobocomp_qmat
+# import librobocomp_osgviewer
+# import librobocomp_innermodel
+
+import math
+
+class SpecificWorker(GenericWorker):
+	def __init__(self, proxy_map):
+		super(SpecificWorker, self).__init__(proxy_map)
+		self.handler = EV3Controller("158.49.227.10", 19998, debug=True)
+		self.timer.timeout.connect(self.compute)
+		self.Period = 2000
+		self.timer.start(self.Period)
+
+	def setParams(self, params):
+		#try:
+		#	self.innermodel = InnerModel(params["InnerModelPath"])
+		#except:
+		#	traceback.print_exc()
+		#	print "Error reading config params"
+		return True
+
+	@QtCore.Slot()
+	def compute(self):
+		print 'SpecificWorker.compute...'
+		print(self.getBasePose())
+		self.correctOdometer(0,0,math.pi)
+		raw_input()
+		return True
+
+
+	#
+	# correctOdometer
+	#
+	def correctOdometer(self, x, z, alpha):
+		"""Reallocate the robot in the map
+		
+		Arguments:
+			x {[int]} -- [X axis position of the robot]
+			z {[int]} -- [Z axis position of the robot]
+			alpha {[int]} -- [Angle of orientation]
+		"""
+		self.handler.move_robot(x, z, alpha)
+	
+
+
+	#
+	# getBasePose
+	#
+	def getBasePose(self):
+		x, z, alpha = self.handler.get_base_pose()
+		return [x, z, alpha]
+
+	#
+	# resetOdometer
+	#
+	def resetOdometer(self):
+		
+		pass
+
+
+	#
+	# setOdometer
+	#
+	def setOdometer(self, state):
+		#
+		#implementCODE
+		#
+		pass
+
+
+	#
+	# getBaseState
+	#
+	def getBaseState(self):
+		#
+		#implementCODE
+		#
+		state = RoboCompGenericBase.TBaseState()
+		return state
+
+
+	#
+	# setOdometerPose
+	#
+	def setOdometerPose(self, x, z, alpha):
+		#
+		#implementCODE
+		#
+		pass
+
+
+	#
+	# stopBase
+	#
+	def stopBase(self):
+		self.handler.set_speed_left(0)
+		self.handler.set_speed_right(0)
+		pass
+
+
+	#
+	# setSpeedBase
+	#
+	def setSpeedBase(self, adv, rot):
+		self.handler.set_global_speed(adv, rot)
+		pass
+	
+	def set_speed_left(self, vel):
+		self.handler.set_speed_left(vel)
+	
+	def set_speed_right(self, vel):
+		self.handler.set_speed_right(vel)
+
+	def get_light_sensors(self):
+		return self.handler.get_light_sensors()
